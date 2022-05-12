cfa2e03254fc935ecfdce796fcc3b00120fc2442
jmsmith1018 <jmsmith1018@gmail.com>
2022-05-12 17:46:26 -0600

    test: add tests for new `self#arriving_at` method on `Flight` model
    This will be able to query the flights that have an arrival airport
    with the corresponding airport code given to the method

--- spec/models/flight_spec.rb
+++ spec/models/flight_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Flight, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "self.arriving_at" do
+    context "when the flight is arriving in San Francisco" do
+      let(:flight_arriving_san_francisco) do
+        create(:flight, :arriving_san_francisco)
+      end
+
+      context "when the given code is San Francisco's Airport code" do
+        it "returns the Flights arriving at San Francisco" do
+          san_francisco_code = "SFO"
+          relation_obj = Flight.arriving_at(san_francisco_code)
+          expect(relation_obj).to include(flight_arriving_san_francisco)
+        end
+      end
+
+      context "when the given code is not San Francisco's Airport code" do
+        it "returns the Flights not including one landing in San Francisco" do
+          denver_code = "DEN"
+          relation_obj = Flight.arriving_at(denver_code)
+          expect(relation_obj).to_not include(flight_arriving_san_francisco)
+        end
+      end
+    end
+  end
+
+  describe "self.departing_from" do
+    xit "returns the Flights departing from the given airport code" do
+      # placeholder
+    end
+  end
 end
