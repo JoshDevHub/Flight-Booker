de6e3dfa87a50868396aa0b45fb078b184afc693
jmsmith1018 <jmsmith1018@gmail.com>
2022-05-12 14:30:00 -0600

    feat: incorporate departure time select into the Flight search

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -5,9 +5,12 @@ class FlightsController < ApplicationController
     departure_airport = Airport.where("airport_code = ?", params[:departure_airport])
     arrival_airport = Airport.where("airport_code = ?", params[:arrival_airport])
 
+    year, month, day = params[:date].values_at(:year, :month, :day)
+    date_search = Date.strptime("#{year} #{month} #{day}", "%Y %m %d")
+
     @flights = Flight.where(
       departure_airport: departure_airport,
       arrival_airport: arrival_airport
-    )
+    ).where("DATE(departure_time) = ?", date_search)
   end
 end
