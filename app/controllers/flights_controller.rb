d972d98f3ee66df2d9780b4aba4fef611290bbbc
jmsmith1018 <jmsmith1018@gmail.com>
2022-05-12 13:38:29 -0600

    refactor: clean up queries
    Likely needs to be on the model, especially as there's more stuff still
    to add.

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,10 +1,13 @@
 class FlightsController < ApplicationController
   def index
-    @airports = Airport.all
+    @airport_codes = Airport.all.map(&:airport_code)
+
+    departure_airport = Airport.where("airport_code = ?", params[:departure_airport])
+    arrival_airport = Airport.where("airport_code = ?", params[:arrival_airport])
 
     @flights = Flight.where(
-      departure_airport_id: params[:departure_airport],
-      arrival_airport_id: params[:arrival_airport]
+      departure_airport: departure_airport,
+      arrival_airport: arrival_airport
     )
   end
 end
