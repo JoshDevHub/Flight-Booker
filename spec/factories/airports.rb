0f3c82d28e40da79bb06b297e18fe15ee469ea38
jmsmith1018 <jmsmith1018@gmail.com>
2022-05-12 17:45:29 -0600

    test: build `Airport` and `Flight` factories for use in testing

--- spec/factories/airports.rb
+++ spec/factories/airports.rb
@@ -1,5 +1,16 @@
 FactoryBot.define do
   factory :airport do
-    airport_code { "MyString" }
+    trait :san_francisco do
+      airport_code { "SFO" }
+    end
+    trait :atlanta do
+      airport_code { "ATL" }
+    end
+    trait :la_guardia do
+      airport_code { "LGA" }
+    end
+    trait :denver do
+      airport_code { "DEN" }
+    end
   end
 end
--- spec/factories/flights.rb
+++ spec/factories/flights.rb
@@ -1,8 +1,16 @@
 FactoryBot.define do
   factory :flight do
     departure_time { "2022-05-10 14:51:46" }
-    flight_duration { 1 }
-    departure_airport { nil }
-    arrival_airport { nil }
+    flight_duration { 3600 }
+    departure_airport { create(:airport) }
+    arrival_airport { create(:airport) }
+
+    trait :arriving_san_francisco do
+      arrival_airport { create(:airport, :san_francisco) }
+    end
+
+    trait :departing_atlanta do
+      departure_airport { create(:airport, :atlanta) }
+    end
   end
 end
