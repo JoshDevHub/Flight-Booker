7e5ab040b2ecf294d5f0bf9b132a44d32046111c
jmsmith1018 <jmsmith1018@gmail.com>
2022-05-13 09:48:41 -0600

    refactor: change date dropdown to leverage formatted departure times

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,12 +1,10 @@
 class FlightsController < ApplicationController
   def index
     @airport_codes = Airport.all.map(&:airport_code)
-
-    year, month, day = params[:date].values_at(:year, :month, :day)
-    date_search = Date.strptime("#{year} #{month} #{day}", "%Y %m %d")
+    @flight_dates = Flight.select(:departure_time).distinct.order(:departure_time)
 
     @flights = Flight.departing_from(params[:departure_airport])
                      .arriving_at(params[:arrival_airport])
-                     .where("DATE(departure_time) = ?", date_search)
+                     .where("departure_time = ?", params[:departure_time])
   end
 end
