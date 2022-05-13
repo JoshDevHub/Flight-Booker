bb4850e8392306ec91d211ff8436ba2804711259
jmsmith1018 <jmsmith1018@gmail.com>
2022-05-13 12:47:27 -0600

    chore: add temporary method to format a label for flights' data

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -13,4 +13,23 @@ class Flight < ApplicationRecord
   def departure_date_formatted
     departure_time.strftime("%m/%d/%Y")
   end
+
+  def display_flight
+    depart_code, arrival_code = [departure_airport, arrival_airport].map(&:airport_code)
+    departure_date = departure_date_formatted
+    "#{depart_code} to #{arrival_code} | #{departure_date} | #{format_duration}"
+  end
+
+  private
+
+  def format_duration
+    seconds = flight_duration
+    format(
+      "%<hour>2d : %<min>2d",
+      {
+        hour: seconds / 3600,
+        min: seconds / 60 % 60
+      }
+    )
+  end
 end
