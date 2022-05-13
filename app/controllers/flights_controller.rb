e0524f30b6feffa895d0882e785686da5cb80a4d
jmsmith1018 <jmsmith1018@gmail.com>
2022-05-13 12:46:28 -0600

    feat: make passenger amount available for flight selection form

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -3,6 +3,7 @@ class FlightsController < ApplicationController
     @airport_codes = Airport.all.map(&:airport_code)
     @flight_dates = Flight.select(:departure_time).distinct.order(:departure_time)
 
+    @passengers = params[:passengers]
     @flights = Flight.departing_from(params[:departure_airport])
                      .arriving_at(params[:arrival_airport])
                      .where("departure_time = ?", params[:departure_time])
