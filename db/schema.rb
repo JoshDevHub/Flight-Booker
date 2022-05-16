47d6513a4f285370f09670eb72729afce0c1f499
jmsmith1018 <jmsmith1018@gmail.com>
2022-05-16 12:37:13 -0600

    feat: create Booking model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_10_205146) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_16_183433) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_10_205146) do
     t.string "airport_code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "flight_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["flight_id"], name: "index_bookings_on_flight_id"
   end
 
   create_table "flights", force: :cascade do |t|
@@ -31,4 +38,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_10_205146) do
     t.index ["departure_airport_id"], name: "index_flights_on_departure_airport_id"
   end
 
+  add_foreign_key "bookings", "flights"
 end
---
+++ spec/factories/bookings.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :booking do
+    flight { nil }
+  end
+end
