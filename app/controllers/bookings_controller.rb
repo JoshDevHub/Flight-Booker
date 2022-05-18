a4083aa6cc99461d7189b443ae69c3ef69861644
jmsmith1018 <jmsmith1018@gmail.com>
2022-05-18 11:46:28 -0600

    feat: configure Bookings controller's `#create` action
    Includes creating a new Booking along with the nested Passenger
    resource.

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -8,7 +8,21 @@ class BookingsController < ApplicationController
   end
 
   def create
-    p params
-    redirect_to root_path
+    @booking = Booking.create(booking_params)
+
+    if @booking.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(
+      :flight_id,
+      passengers_attributes: %i[id first_name last_name email]
+    )
   end
 end
