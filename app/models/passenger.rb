37df89855ecb4beb2609fa0c9acf70e4e864fba3
jmsmith1018 <jmsmith1018@gmail.com>
2022-05-24 20:01:31 -0600

    feat: add validations for `Passenger` info

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,3 +1,7 @@
 class Passenger < ApplicationRecord
   belongs_to :booking
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
 end
