d05bc8c92c153ebce01176b7fabf246cdddadebb
jmsmith1018 <jmsmith1018@gmail.com>
2022-05-26 10:59:34 -0600

    fix: use `Flight::list_formatted_departure_dates` in Controller/Views
    Previous implementation was duplicating flight dates in the dropdown
    list. This commit along with the new method fixes this problem.

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,11 +1,13 @@
 class FlightsController < ApplicationController
   def index
     @airport_codes = Airport.all.map(&:airport_code)
-    @flight_dates = Flight.select(:departure_time).distinct.order(:departure_time)
+    @flight_dates = Flight.list_formatted_departure_dates
 
     @passengers = params[:passengers]
     @flights = Flight.departing_from(params[:departure_airport])
                      .arriving_at(params[:arrival_airport])
-                     .where(departure_time: params[:departure_time])
+                     .where(
+                       "DATE(departure_time) = ?", params[:departure_time]&.[](:date)
+                     )
   end
 end
