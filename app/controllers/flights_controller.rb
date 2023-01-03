ff1006fca8db4e86b8d3870fc91ad551ac0bd3df
jmsmith1018 <jmsmith1018@gmail.com>
2023-01-03 14:54:10 -0700

    fix: error with passenger param nesting

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,7 +2,7 @@ class FlightsController < ApplicationController
   def index
     @airport_codes = Airport.all.map(&:airport_code)
     @flight_dates = Flight.list_formatted_departure_dates
-    @passengers = params[:passengers]
+    @passengers = params.dig(:search, :passengers)
 
     @flights = Flight.search(search_params)
   end
